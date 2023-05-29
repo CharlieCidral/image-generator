a56498e2d6e0c484537ce447cb7ff650820fec54
Charlie Samoel <69029099+CharlieCidral@users.noreply.github.com>
2023-05-29 17:56:11 -0300

    Update index.js
    update connect

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const startServer = async () => {
 
     try {
         connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+        app.listen(5000, () => console.log('Server has started on port http://localhost:5000'))
     } catch (error) {
         console.log(error);
     }
